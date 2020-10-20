c530cf3fe55e6b540d5f4895017b3ae4267050d7
Tyler Klafke <Ttylerrk@gmail.com>
2020-10-20 08:50:34 -0700

    make pokemon sprites and names visible

--- poke.js
+++ poke.js
@@ -1,8 +1,8 @@
 const POKE_URL = `https://pokeapi.co/api/v2/pokemon/?limit=893`
-let singlePokeURL =  `https://pokeapi.co/api/v2/pokemon/${1}`
+let singlePokeURL =  `https://pokeapi.co/api/v2/pokemon/`
 // console.log(singlePokeURL)
 pokeAmount = 893
-let pokeDiv = document.querySelector('#poke_div')
+let allPokeDiv = document.querySelector('.poke_div')
 
 //========================================================================================================================================
 
@@ -11,16 +11,32 @@ let pokeDiv = document.querySelector('#poke_div')
 //BELOW IS FOR ALL POKEMON SEARCH, BUT IT ONLY GIVES ME THE NAMES, AND NOT EACH INDIVIDUAL CHARACTERISTICS
 
 const allPokeJson = () => {
-fetch(POKE_URL)
-.then(response => {
-    return response.json()
-})
-.then(data => {
+    axios.get(POKE_URL)
+    .then(response => {
+        console.log(response.data.results)
+        //result is going to be 1 big array with length 893
+        let pokeArray = response.data.results
+        for(let i=0; i < pokeArray.length; i++){
+            let link = document.createElement('a')
+            link.href = './pokemon.html'
+            let pokeDiv= document.createElement("div")
+            let pokeName = document.createElement('h2')
+            let sprite = document.createElement('img')
+            pokeName.innerText = pokeArray[i].name
+            sprite.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i + 1}.png`
+            link.appendChild(pokeName)
+            pokeDiv.appendChild(link)
+            pokeDiv.appendChild(sprite)
+            allPokeDiv.appendChild(pokeDiv)
+            link.addEventListener('click', () => onePokeJson(singlePokeURL + `${pokeArray[i].name}`))
 
-    console.log(data)    
-})
+           
+            
+        }
+    })
+    .catch(error => console.log("ERROR: ", error))
 }
-allPokeJson()
+
 
 
 //========================================================================================================================================
@@ -29,20 +45,13 @@ allPokeJson()
 
 //BELOW IS FOR A SINGLE POKEMON SEARCH, WHICH GIVES ME TONS OF INFO FROM THAT ONE POKEMON
 
-const onePokeJson = () => {
-    fetch(singlePokeURL)
+const onePokeJson = (url) => {
+    axios.get(url)
     .then(response => {
-        return response.json()
+        console.log(response.data)
     })
-    .then(data => {
-    
-        console.log(data) 
-        const pokemon = {}
-        pokemon.name = data.name
-        console.log(pokemon) 
-    })
-    }
-    onePokeJson()
+   .catch(error => console.log("ERROR: ", error))
+}
 
 
 
@@ -52,14 +61,13 @@ const onePokeJson = () => {
 
 // THIS IS A FOR LOOP THAT CREATES THE 893 DIVS FOR EACH OF THE POKEMON
 
-for (let i = 0; i <= pokeAmount; i++) {
-    let pokeDiv = document.createElement('div')
-    pokeDiv.classList.add('.poke_div')
-    pokeDiv.addEventListener('click', function(event){
-        pokeDiv.style.backgroundColor= green
-    })
-    document.body.appendChild(pokeDiv)
-}
+// for (let i = 0; i <= pokeAmount; i++) {
+//     let pokeDiv = document.createElement('div')
+//     pokeDiv.classList.add('.poke_div')
+//     })
+//     document.body.appendChild(pokeDiv)
+// }
+
 
 //========================================================================================================================================
 
@@ -75,3 +83,4 @@ for (let i = 0; i <= pokeAmount; i++) {
 //organize the divs through flexbox. (wrapping?)
 //add an event listener to each div, to where when I click on a div, the background will disappear.
 //
+document.addEventListener('DOMContentLoaded', allPokeJson)
